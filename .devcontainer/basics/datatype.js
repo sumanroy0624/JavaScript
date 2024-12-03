dc376d79e5f3ed5f83670fe4c059a0102f56d39f
Suman Roy <138618510+sumanroy0624@users.noreply.github.com>
2024-12-03 20:27:40 +0000

    data typer

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,15 @@
+"use strict" //treat all JS code as newer version
+// alert(3+3) // we are using node not using browser
+
+let name="suman" // string
+let n=1 //number 2^53
+let flag=true //boolean
+let x=null //null -> standalone value
+// undefine -> 
+// symbol ->unique
+
+//object 
+
+console.log(typeof(name)); 
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
