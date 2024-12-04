4cc25e546610a28add43851148596a54311f2532
Suman Roy <138618510+sumanroy0624@users.noreply.github.com>
2024-12-04 20:30:16 +0000

    strings

---
+++ .devcontainer/basics/11_Strings.js
@@ -0,0 +1,6 @@
+const myname="suman";
+const age=20;
+console.log("my name is "+ myname + " and my age is "+age);  //not preferable
+
+console.log(`my name is ${myname} and my age is ${age}`); //prefeable
+
