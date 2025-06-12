338a2f8d72a40b2bf0091b7d0ce057a0b07e6b1f
Suman Roy <138618510+sumanroy0624@users.noreply.github.com>
2025-06-12 16:19:06 +0530

    Delete .devcontainer directory

--- .devcontainer/basics/08_comparison.js
+++
@@ -1,6 +0,0 @@
-console.log(null>0);
-// === check data and datatype
-
-console.log("2"==2); // true
-console.log("2"===2); //false
-
--- .devcontainer/basics/conversion_operation2.js
+++
@@ -1,5 +0,0 @@
-let value=3
-let negVal=-value;
-console.log(negVal);
-// console.log(+true);
-console.log(+"");
